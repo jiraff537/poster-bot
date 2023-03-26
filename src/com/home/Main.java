f86c92221d75c544e5ebcb1bf49e0499a60d472e
Igor <gogy913@gmail.com>
2023-03-26 21:48:25 +0300

    changed Hitrov to Khitrov

--- src/com/home/Main.java
+++ src/com/home/Main.java
@@ -9,19 +9,20 @@ public class Main {
     }
 
     private static void justInitConsole() {
-        System.out.println("                                                                            \n" +
-                "                                                                            \n" +
-                "`7MM\"\"\"Mq.                    mm                 `7MM\"\"\"Yp,           mm    \n" +
-                "  MM   `MM.                   MM                   MM    Yb           MM    \n" +
-                "  MM   ,M9 ,pW\"Wq.  ,pP\"Ybd mmMMmm .gP\"Ya `7Mb,od8 MM    dP  ,pW\"Wq.mmMMmm  \n" +
-                "  MMmmdM9 6W'   `Wb 8I   `\"   MM  ,M'   Yb  MM' \"' MM\"\"\"bg. 6W'   `Wb MM    \n" +
-                "  MM      8M     M8 `YMMMa.   MM  8M\"\"\"\"\"\"  MM     MM    `Y 8M     M8 MM    \n" +
-                "  MM      YA.   ,A9 L.   I8   MM  YM.    ,  MM     MM    ,9 YA.   ,A9 MM    \n" +
-                ".JMML.     `Ybmd9'  M9mmmP'   `Mbmo`Mbmmd'.JMML. .JMMmmmd9   `Ybmd9'  `Mbmo \n" +
-                "                                                                            \n" +
-                "                                                                            ");
+        System.out.println("""
+                                                                                           \s
+                                                                                           \s
+                `7MM""\"Mq.                    mm                 `7MM""\"Yp,           mm   \s
+                  MM   `MM.                   MM                   MM    Yb           MM   \s
+                  MM   ,M9 ,pW"Wq.  ,pP"Ybd mmMMmm .gP"Ya `7Mb,od8 MM    dP  ,pW"Wq.mmMMmm \s
+                  MMmmdM9 6W'   `Wb 8I   `"   MM  ,M'   Yb  MM' "' MM""\"bg. 6W'   `Wb MM   \s
+                  MM      8M     M8 `YMMMa.   MM  8M""\"""\"  MM     MM    `Y 8M     M8 MM   \s
+                  MM      YA.   ,A9 L.   I8   MM  YM.    ,  MM     MM    ,9 YA.   ,A9 MM   \s
+                .JMML.     `Ybmd9'  M9mmmP'   `Mbmo`Mbmmd'.JMML. .JMMmmmd9   `Ybmd9'  `Mbmo\s
+                                                                                           \s
+                                                                                           \s""");
         System.out.println("01010000 01101111 01110011 01110100 01100101 01110010 01000010 01101111 01110100 ");
-        System.out.println("(c) 2023 Alexei A Danilov, Igor A Hitrov");
+        System.out.println("(c) 2023 Alexei A Danilov, Igor A Khitrov");
 
     }
 }
